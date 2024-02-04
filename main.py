ef13ee3d635648ba4abec8e949931973b00a6d3d
ducanhkhuong <duca3774@gmail.com>
2024-02-04 20:05:28 +0700

    add Algorithm

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
-import random
+a = []
+var = input("nhap so phan tu cua List :  ")
 
-chuoi = input("nhap chuoi : ")
-danh_sach_phan_tu = list(chuoi)
-random.shuffle(danh_sach_phan_tu)
-chuoi_ngau_nhien = ''.join(danh_sach_phan_tu)
-print("Chuỗi gốc:", chuoi)
-print("Chuỗi sau khi được ngẫu nhiên hoán đổi:", chuoi_ngau_nhien)
+def nhap_mang():
+   for i in range(0,int(var)):
+      i = input("nhap vao vi tri " + str(i) + " : ")
+      a.append(i)
+
+def xuat_mang():
+   print("List vua nhap la : " + str(a))
+
+
+def check_nguyen_to(n):
+   if (n < 2):
+      return False;
+   for i in range(2, n):
+      if (n % i == 0):
+         return False
+   return True
+
+
+def check_chinh_phuong(x):
+   if (x < 1):
+      return False;
+   i = 1
+   while (i * i <= x):
+      if (i * i == x):
+         return True;
+      i = i + 1
+   return False
+
+def so_nguyen_to():
+   sum = 0
+   for i in range(0, int(var)):
+     if (check_nguyen_to(int(a[i]))):
+      print("cac phan tu nguyen to trong List : " + str(a[i]))
+      sum = sum + 1
+   print("Tong phan tu nguyen to trong List : " + str(sum))
+
+
+def so_chinh_phuong():
+   for j in range(0,int(var)):
+      if(check_chinh_phuong(int(a[j]))):
+         print("cac phan tu chinh phuong tronh List : " + str(a[j]))
+
+
+
+ 
+
+if __name__ == "__main__":
+    nhap_mang()
+    xuat_mang()
+    so_nguyen_to()
+    so_chinh_phuong()
