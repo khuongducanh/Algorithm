1e5140d516ab24ce45f150a4279c30ca9d7248da
ducanhkhuong <duca3774@gmail.com>
2024-02-04 13:15:54 +0700

    add PrimeNumber

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def check(x) :
-    if(x<0):
-        return False
-    if(x%2==0):
-        return True
-    return False        
+import math
 
+def isPrimeNumber(n):
+    # so nguyen n < 2 khong phai la so nguyen to
+    if (n < 2):
+        return False
+    # check so nguyen to khi n >= 2
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+    return True
 
 if __name__ == "__main__":
-   for i in range (100):
-    if(check(i)==True):
-        print(i)
+    listPrimeNumber = []
+    print("Cac so nguyen to nho hon 100 la:")
+    for i in range(0, 100):
+        if (isPrimeNumber(i)):
+            print(i)
