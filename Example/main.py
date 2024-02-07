d4a4b5d6ab9df8e145870cb8162fb7dada26309a
ducanhkhuong <duca3774@gmail.com>
2024-02-07 09:39:58 +0700

    add example

--- Example/main.py
+++ Example/main.py
@@ -3,7 +3,7 @@ from Lib import *
 
 if __name__ == '__main__' :
     sorted_array = [1, 2, 2, 2, 3, 4, 5, 6] #=> The array has been sorted
-    result = binary_search(sorted_array, 6)
+    result = linear_search(sorted_array, 6)
     if result != -1:
         print(f"value 2 in index : {result}")
     else:
